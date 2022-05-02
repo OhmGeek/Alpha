69179b2b385c32c9d9cc13718abc7963109b3e5b
Ryan Collins <ryan@ohmgeek.co.uk>
2022-05-02 12:05:19 +0100

    Add tests for error cases. These are mostly wrong, but we will adjust as required

--- src/lib.rs
+++ src/lib.rs
@@ -6,13 +6,23 @@ pub struct Cpu {
     // general purpose registers
     pub r: [u64; 32],
     // floating point registers
-    pub f: [u64; 32]
+    pub fr: [u64; 32],
 }
 
 impl Cpu {
+    // Called each clock cycle. This handles the CPU emulation, handling a single instruction.
+    // There is no pipelining implemented.
     pub fn execute_cycle(&mut self) {
         // TODO fetch the state from memory
         // TODO process the opcode
-        self.pc += 1
+        let instr = self.memory[self.pc as usize];
+        self.handle_op(instr);
+    }
+
+    fn handle_op(&mut self, op: u64) {
+        match op {
+            0 => self.pc += 1,
+            _ => panic!("Error, unimplemented opcode {}", op),
+        }
     }
 }
--- tests/cpu_fetch_execute_test.rs
+++ tests/cpu_fetch_execute_test.rs
@@ -2,12 +2,29 @@ use alpha::Cpu;
 
 #[test]
 fn it_increments_program_counter() {
+    // Given we read an opcode of zero (and pc is 0)
     let mut cpu = Cpu {
         pc: 0,
         memory: [0; 4096],
         r: [0;32],
-        f: [0;32]
+        fr: [0;32]
     };
+    // When we execute
     cpu.execute_cycle();
+    // Then we should increase the program counter
     assert_eq!(cpu.pc, 1);
+}
+
+#[test]
+#[should_panic]
+fn it_handles_unimplemented_opcode() {
+    // Given we read an opcode of 1 which isn't implemented
+    let mut cpu = Cpu {
+        pc: 0,
+        memory: [1; 4096],
+        r: [0;32],
+        fr: [0;32]
+    };
+    // When we execute, expect the test to panic
+    cpu.execute_cycle();
 }

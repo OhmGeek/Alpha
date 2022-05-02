f5782036a5e2df93daed22b556e1daaf96c234db
Ryan Collins <ryan@ohmgeek.co.uk>
2022-05-02 12:22:18 +0100

    Add initial ops, as following es40 emulator

--- src/lib.rs
+++ src/lib.rs
@@ -13,16 +13,37 @@ impl Cpu {
     // Called each clock cycle. This handles the CPU emulation, handling a single instruction.
     // There is no pipelining implemented.
     pub fn execute_cycle(&mut self) {
-        // TODO fetch the state from memory
-        // TODO process the opcode
         let instr = self.memory[self.pc as usize];
-        self.handle_op(instr);
+        self.handle_instruction(instr);
     }
 
-    fn handle_op(&mut self, op: u64) {
+    fn handle_instruction(&mut self, op: u64) {
+        // TODO we should group ops into various files, so we can test them individually.
+        // TODO don't use the op itself, but break it into <opcode> and <data>
+        // Based on this, pattern match only the opcode rather than the full instruction as we're doing now.
         match op {
-            0 => self.pc += 1,
+            0x00 => self.pc += 1,
+            0x08 => self.op_lda(op),
+            0x09 => self.op_ldah(),
+            0x0a => self.op_ldbu(),
             _ => panic!("Error, unimplemented opcode {}", op),
         }
+    }
+
+    fn call_pal(&mut self) {
+        // stub
+    }
+
+    fn op_lda(&mut self, op: u64) {
+        // r[1] = r[2]
+        // TODO: take the op, and figure out which two registers to move.
+    }
+
+    fn op_ldah(&mut self) {
+        // stub
+    }
+
+    fn op_ldbu(&mut self) {
+        // stub
     }
 }

559db40352f115c33d4bc6cc04846c5c1fd8ab26
Ryan Collins <ryan@ohmgeek.co.uk>
2022-05-07 09:27:13 +0100

    LDA implemented

--- src/lib.rs
+++ src/lib.rs
@@ -4,8 +4,10 @@ pub struct Cpu {
     // memory: should follow configured memory sizes
     pub memory: [u64; 4096],
     // general purpose registers
+    // R32 is always zero (we're doing a Python2 here and trusting the user)
     pub r: [u64; 32],
     // floating point registers
+    // F32 is always set to 0.0 (again, we are very trusting)
     pub fr: [u64; 32],
 }
 
@@ -17,13 +19,17 @@ impl Cpu {
         self.handle_instruction(instr);
     }
 
-    fn handle_instruction(&mut self, op: u64) {
+    fn handle_instruction(&mut self, inst: u64) {
+        // Get the instruction from the opcode
+        let op = get_opcode(inst);
+
         // TODO we should group ops into various files, so we can test them individually.
         // TODO don't use the op itself, but break it into <opcode> and <data>
         // Based on this, pattern match only the opcode rather than the full instruction as we're doing now.
+        println!("Looking at opcode {}", op);
         match op {
-            0x00 => self.pc += 1,
-            0x08 => self.op_lda(op),
+            // 0x00 => self.pc += 1,
+            0x08 => self.op_lda(inst),
             0x09 => self.op_ldah(),
             0x0a => self.op_ldbu(),
             _ => panic!("Error, unimplemented opcode {}", op),
@@ -34,9 +40,11 @@ impl Cpu {
         // stub
     }
 
-    fn op_lda(&mut self, op: u64) {
-        // r[1] = r[2]
-        // TODO: take the op, and figure out which two registers to move.
+    fn op_lda(&mut self, inst: u64) {
+        let ra = get_ra(inst);
+        let rb = get_rb(inst);
+        self.r[rb as usize] = self.r[ra as usize];
+        println!("lda from r{} to r{}", ra, rb)
     }
 
     fn op_ldah(&mut self) {
@@ -46,4 +54,16 @@ impl Cpu {
     fn op_ldbu(&mut self) {
         // stub
     }
+}
+
+fn get_ra(inst: u64) -> u64 {
+    return (inst >> 21) & 0x1F;
+}
+
+fn get_rb(inst: u64) -> u64 {
+    return (inst >> 16) & 0x1F;
+}
+
+fn get_opcode(inst: u64) -> u64 {
+    return (inst >> 26) & 0x3F;
 }
--- tests/cpu_fetch_execute_test.rs
+++ tests/cpu_fetch_execute_test.rs
@@ -21,10 +21,40 @@ fn it_handles_unimplemented_opcode() {
     // Given we read an opcode of 1 which isn't implemented
     let mut cpu = Cpu {
         pc: 0,
-        memory: [1; 4096],
+        memory: [0xFFFFFFFF; 4096],
         r: [0;32],
         fr: [0;32]
     };
     // When we execute, expect the test to panic
     cpu.execute_cycle();
+}
+
+#[test]
+fn it_should_handle_lda_with_zero_address_displacement() {
+    //Init the CPU subsystem
+    let mut cpu = Cpu {
+        pc: 0,
+        memory: [0; 4096],
+        r: [0;32],
+        fr: [0;32]
+    };
+
+    // LDA opcode
+    cpu.memory[0] = to_inst(0x08, 0x01, 0x02);
+    cpu.r[1] = 0x5;
+    assert_eq!(0, cpu.r[2]);
+
+    // After we execute, expect the data to have been loaded.
+    cpu.execute_cycle();
+    assert_eq!(0x5, cpu.r[2]);
+}
+
+fn to_inst(opcode: u64, ra: u64, rb: u64) -> u64 {
+    // First 6 bits are opcode
+    let opcode_part = (opcode & 0x3F) << 26;
+    let ra_part = (ra & 0x1F) << 21;
+    let rb_part = (rb & 0x1F) << 16;
+
+    // OR the bits together, creating the full instruction
+    return opcode_part | ra_part | rb_part;
 }
